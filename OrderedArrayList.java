78653d71ae15d28e22651a4982940563980860b5
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2020-12-09 20:11:04 -0500

    Started file and added constructors

---
+++ OrderedArrayList.java
@@ -0,0 +1,10 @@
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+
+  public OrderedArrayList(){
+    super();
+  }
+
+  public OrderedArrayList(int startingCapacity) {
+    super(startingCapacity);
+  }
+}
