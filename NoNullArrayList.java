10a256776f05454d4e4058853edb7cad7781961b
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2020-12-09 19:59:37 -0500

    made set, and both add methods. They all throw IllegalArgumentException is null if added, and call the parent method if not

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -2,4 +2,19 @@ import java.util.*;
 
 public class NoNullArrayList<T> extends ArrayList<T> {
 
+  set(int index, T element){
+    if (element.equals(null)) throw IllegalArgumentException;
+    else super.set(index, element);
+  }
+
+  add(int index, T element) {
+    if (element.equals(null)) throw IllegalArgumentException;
+    else super.add(index, element);
+  }
+
+  add(T e) {
+    if (e.equals(null)) throw IllegalArgumentException;
+    else super.add(e);
+  }
+
 }
