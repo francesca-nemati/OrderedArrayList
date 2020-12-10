4480852a708da08db7ad70c0fe161a4afbcbe262
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2020-12-09 20:03:05 -0500

    added types and public to methods

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 public class NoNullArrayList<T> extends ArrayList<T> {
 
-  set(int index, T element){
+  public T set(int index, T element){
     if (element.equals(null)) throw IllegalArgumentException;
     else super.set(index, element);
   }
 
-  add(int index, T element) {
+  public void add(int index, T element) {
     if (element.equals(null)) throw IllegalArgumentException;
     else super.add(index, element);
   }
 
-  add(T e) {
+  public boolean add(T e) {
     if (e.equals(null)) throw IllegalArgumentException;
     else super.add(e);
   }
