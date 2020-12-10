17f2943073b3c35a3f8f0e1b4d5234efc35c9ad5
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2020-12-10 13:06:48 -0500

    added add methods

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -7,4 +7,27 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int startingCapacity) {
     super(startingCapacity);
   }
+
+  public void add(int index, T element) {
+    for (int i = 0; i < size(); i++) {
+      if (e.compareTo(get(i)) <= 0) {
+        super.add(i,e);
+      }
+    }
+    super.add(size(),e);
+  }
+
+  public boolean add(T e) {
+    for (int i = 0; i < size(); i++) {
+      if (e.compareTo(get(i)) <= 0) {
+        super.add(i,e);
+      }
+    }
+    super.add(size(),e);
+  }
+
+  public T set(int index, T element){
+
+  }
+
 }
